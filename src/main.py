7f41b8bbc28e1e57a819aa461e52515a23df6bf9
Vafantule <pavelgav@gmail.com>
2025-04-19 15:20:48 +0500

    Практика задания 9.1.4

--- src/main.py
+++ src/main.py
@@ -6,20 +6,23 @@
 
 from typing import List
 
+
 def get_sort_numbers(list_1: List[int], list_2: List[int]) -> List[int]:
     result = list(filter(lambda elem: elem in list_1, list_2))
     return result
     # return list(set(list_1) & set(list_2))
 
+
 # if __name__ == "__main__":
-    # print(get_sort_numbers([1, 2, 3, 4], [3, 4, 5, 6]))
+# print(get_sort_numbers([1, 2, 3, 4], [3, 4, 5, 6]))
 
 
 """
-Написать функцию, 
-которая получает на вход список чисел и возвращает новый список, 
-содержащий только числа, которые являются палиндромами.
+Написать функцию,
+которая получает на вход список чисел и возвращает новый список,
+содержащий только числа, которые являются палиндромами
 """
+
 
 def palindrome_list(unformatted_string: List[int]) -> List[int]:
     # strint_lower = strint.lower()
@@ -32,12 +35,12 @@ def palindrome_list(unformatted_string: List[int]) -> List[int]:
 
 
 # if __name__ == "__main__":
-    # print(palindrome_list([121, 123, 131, 34543]))
+# print(palindrome_list([121, 123, 131, 34543]))
 
 
 """
-Написать функцию, 
-которая получает на вход два списка чисел и возвращает новый список, 
+Написать функцию,
+которая получает на вход два списка чисел и возвращает новый список,
 содержащий только те числа, которые есть только в одном из списков.
 """
 
@@ -48,34 +51,46 @@ def get_not_sort_numbers(list_1: List[int], list_2: List[int]) -> List[int]:
     list_3 = list(set(list_1) - set(list_2)) + list(set(list_2) - set(list_1))
     return list_3
 
+
 # if __name__ == "__main__":
 #     print(get_not_sort_numbers([1, 2, 3, 4], [3, 4, 5, 6]))
 
 
 """
-Исправьте код, содержащий ошибки PEP 8 и плохой нейминг. 
-Добавьте docstring и аннотации типов. Flake8 и mypy 
+Исправьте код, содержащий ошибки PEP 8 и плохой нейминг.
+Добавьте docstring и аннотации типов. Flake8 и mypy
 не должны выдавать ошибок.
 """
 
 
-def circle_area(radius: int) -> int:
+def circle_area(radius: float) -> float:
     """
-    Вычисление радиуса по формуле. Функция.
+    Вычисление площади круга по формуле. Функция.
     :param radius:
     :return:
     """
-    PI = 3.14
-    circleArea = PI * radius ** 2
-    return circleArea
+    pi = 3.14
+    circle_area = pi * radius**2
+    return circle_area
 
-def format_description(radius, area):
+
+def format_description(radius: float, area: float):
+    """
+    Отформатированный вывод площади круга.
+    """
     return "Radius is " + str(radius) + "; area is " + str(round(area, 2))
 
-def get_info(radius: float):
+
+def get_info(radius: float) -> None:
+    """
+    Вывод информации?
+    :param radius:
+    :return:
+    """
     area = circle_area(radius)
     description = format_description(radius, area)
     print(description)
 
-radius_final = int(input("Enter circle radius (int): "))
-get_info(radius_final)
+# if __name__ == "__main__":
+    radius_final = int(input("Enter circle radius (int): "))
+    get_info(radius_final)
