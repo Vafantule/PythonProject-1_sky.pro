01dddee726a8d311b3946b0aa1a6c476c247fba9
Vafantule <pavelgav@gmail.com>
2025-04-24 22:57:09 +0500

    modify module src/processing. change module

--- src/processing.py
+++ src/processing.py
@@ -3,23 +3,23 @@ from typing import Any, Dict, List
 
 def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
-    Отбор данных по заданному значению. Функция.
+    Возвращает новый список, отфильтрованный по указанному значению. Функция.
     :param transactions: Данные.
-    :param state: Отбор значения.
+    :param state: Выбор значения, для отбора.
     :return: Возвращает список по заданной переменной.
     """
     select_value = [value for value in transactions if value.get("state") == state]
     return select_value
 
 
-def sort_by_date(transactions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+def sort_by_date(transactions: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """
     Осуществляет сортировку данных по заданной переменной. Функция.
     :param transactions: Данные.
     :param reverse: Направление сортировки.
     :return: Возвращает отсортированный список по заданной переменной.
     """
-    sort_date = sorted(transactions, key=lambda sort_type: sort_type["date"], reverse=True)
+    sort_date = sorted(transactions, key=lambda sort_type: str(sort_type["date"]), reverse=reverse)
     return sort_date
 
 
