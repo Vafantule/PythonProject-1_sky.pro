196d0500438b100551aa2ca1eadeb250a624a3e7
Vafantule <pavelgav@gmail.com>
2025-04-24 12:30:20 +0500

    modify module src/processing. add def filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,24 +1,16 @@
 from typing import List, Dict
 
-def filter_by_state(state: str="EXECUTED") -> str:
-    pass
+def filter_by_state(select_values, state="EXECUTED"):
+    select_value = [value for value in select_values if value.get("state") == state]
+    return select_value
 
 
-
-
-
-
-
-
-
-
-
-def sort_by_date(sort_types: List[dict], reverse:bool=True) -> List[dict]:
+def sort_by_date(sort_types: List[Dict], reverse: bool=True) -> List[Dict]:
     """
     Осуществляет сортировку данных по заданной переменной. Функция.
     :param sort_types: Данные.
     :param reverse: Направление сортировки.
     :return: Возвращает отсортированный список по заданной переменной.
     """
-    sort_date = sorted(sort_types, key=lambda sort_type: sort_type.get("date"), reverse=reverse)
+    sort_date = sorted(sort_types, key=lambda sort_type: sort_type.get("date"), reverse=True)
     return sort_date
