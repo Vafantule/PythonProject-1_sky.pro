7c73935e58fd226b346b4c92b32d8bc717dbde72
Vafantule <pavelgav@gmail.com>
2025-05-06 16:12:37 +0500

    some changes to widget

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
-import re
 import datetime
-from src.masks import get_mask_card_number, get_mask_account
+import re
 
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(string_to_mask: str) -> str:
