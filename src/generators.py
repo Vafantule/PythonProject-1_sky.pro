77a9fd1468bd653863f9a37bb5ab1fdf4d9123d8
Vafantule <pavelgav@gmail.com>
2025-05-10 17:38:29 +0500

    add function transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Generator
+from typing import Any, Dict, List, Generator, Iterator, Optional
 
 transactions = (
     [
@@ -94,29 +94,50 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -
         raise ValueError("Транзакции должны быть представлены в виде словаря.")
 
     for transaction in transactions:
-        currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
+        currency = transaction.setdefault("operationAmount").get("currency").get("code")
         if currency == currency_code:
             yield transaction
 
 
 # Получить итератор для транзакций в валюте.
-# usd_transactions = filter_by_currency(transactions, "USD")
-# for _ in range(3):
-#     print(next(usd_transactions))
+usd_transactions = filter_by_currency(transactions, "USD")
+# while True:
+# try:
+#     for _ in range(6):
+#         print(next(usd_transactions))
+# except StopIteration:
+#     print("Окончание итерации.")
+    # break
 
 
-def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Generator[Any | None, Any, None]:
+def transaction_descriptions(transactions: List[Dict[str, Optional[Any]]]) -> Iterator[str]:
     """
     Вывод описания операций по транзакциям. Функция.
     :param transactions: Данные для отбора.
     :return:
+    :param transactions:
+    :return:
     """
-    first_step = (transaction.setdefault("description") for transaction in transactions)
-    return first_step
+    if not isinstance(transactions, list):
+        raise TypeError("Просьба предоставить корректный тип данных.")
+    for transaction in transactions:
+        if not isinstance(transaction, dict):
+            continue
+        description = transaction.setdefault("description", "Нет необходимого словаря.")
+        if isinstance(description, str) and description:
+            yield description
+        else:
+            yield "Нет необходимого словаря."
 
-# descriptions = transaction_descriptions(transactions)
-# for _ in range(2):
-#     print(next(descriptions))
+
+descriptions = transaction_descriptions(transactions)
+# while True:
+try:
+    for _ in range(9):
+        print(next(descriptions))
+except StopIteration:
+    print("Окончание итерации.")
+    # break
 
 
 def luna_check(card_number: str) -> bool:
@@ -146,6 +167,15 @@ def card_number_generator(start: int, end: int) -> Generator[str, Any, None]:
     :param end: Конечное значение номера карты.
     :return: Отформатированный вывод номера карты.
     """
+    if start < 0:
+        raise ValueError("Начальное число не может быть меньше или равно 0.")
+
+    if start > end:
+        raise ValueError("Начальный номер должен быть меньше или равен конечному.")
+
+    if not (0 <= start <= 9999999999999999 and 0 <= end <= 9999999999999999):
+        raise ValueError("Номера карт должны быть в диапазоне от 0 до 9999999999999999.")
+
     for number in range(start, end + 1):
         card_number = f"{number:016}"
     # if luna_check(card_number):
@@ -155,5 +185,5 @@ def card_number_generator(start: int, end: int) -> Generator[str, Any, None]:
 
 
 # Вывод номера карты в выбранном диапазоне.
-# for card_number in card_number_generator(1, 5):
+# for card_number in card_number_generator(1, 7):
 #     print(card_number)
