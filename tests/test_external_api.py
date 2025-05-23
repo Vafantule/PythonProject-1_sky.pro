9fff6755168faadc723fab43dd9b2a23689452af
Vafantule <pavelgav@gmail.com>
2025-05-23 23:16:50 +0500

    add tests for function def get_exchange_rate() & def get_api_key()

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,11 +1,14 @@
 from typing import Any, Dict
 from unittest.mock import Mock, patch
+from urllib.error import HTTPError
 
 import pytest
+from requests import ConnectionError, HTTPError, Timeout, RequestException
 
-from src.external_api import get_transaction_amount, get_api_key
+from src.external_api import get_transaction_amount, get_api_key, get_exchange_rate
 
 
+# Блок тестирования вывода валюты в рублях функции def get_transaction_amount()
 @pytest.fixture
 def transaction_rub() -> Dict[str, Any]:
     """
@@ -133,3 +136,87 @@ def test_read_from_file(
     mock_json_load.return_value = transaction_rub
     result = get_transaction_amount("fake_file.json")   # type: ignore
     assert result == 42210.20
+
+
+# Блок тестирования запроса API функции def get_api_key()
+@patch("src.external_api.os.getenv")
+@patch("src.external_api.load_dotenv")
+def test_get_api_key_found(mock_load_dotenv: Mock, mock_getenv: Mock) -> None:
+    """
+
+    :param mock_load_dotenv:
+    :param mock_getenv:
+    :return:
+    """
+    mock_getenv.return_value = "TEST_KEY"
+    assert get_api_key() == "TEST_KEY"
+    mock_load_dotenv.assert_called_once()
+    mock_getenv.assert_called_with("EXCHANGE_RATES_API_KEY", "")
+
+
+@patch("src.external_api.os.getenv")
+@patch("src.external_api.load_dotenv")
+def test_get_api_key_not_found(mock_load_dotenv: Mock, mock_getenv: Mock) -> None:
+    """
+
+    :param mock_load_dotenv:
+    :param mock_getenv:
+    :return:
+    """
+    mock_getenv.return_value = ""
+    with pytest.raises(ValueError, match="Ключ EXCHANGE_RATES_API_KEY не найдет в .env файле."):
+        get_api_key()
+    mock_load_dotenv.assert_called_once()
+    mock_getenv.assert_called_with("EXCHANGE_RATES_API_KEY", "")
+
+
+# Блок тестирования курса валюты функции def get_exchange_rate()
+@patch("requests.get")
+def test_get_exchange_rate_success(mock_get: Mock) -> None:
+    """
+
+    :param mock_get:
+    :return:
+    """
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"rates": {"RUB": 99.99}}
+    mock_get.return_value = mock_response
+
+    rate = get_exchange_rate("USD", "RUB", "some_key")
+    assert rate == 99.99
+
+
+@patch("requests.get")
+def test_get_exchange_rate_not_found(mock_get: Mock) -> None:
+    """
+
+    :param mock_get:
+    :return:
+    """
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"rates": {"EUR": 1.12}}
+    mock_get.return_value = mock_response
+
+    with pytest.raises(KeyError):
+        get_exchange_rate("USD", "RUB", "some_key")
+
+
+@patch("requests.get")
+@pytest.mark.parametrize("exc", [
+    ConnectionError("ConnectionError"),
+    HTTPError("HTTPError"),
+    Timeout("Timeout"),
+    RequestException("RequestException")
+])
+def test_get_exchange_rate_not_requests_exceptions(mock_get: Mock, exc: Exception):
+    """
+
+    :param mock_get:
+    :param exc:
+    :return:
+    """
+    mock_get.side_effect = exc
+    with pytest.raises(type(exc)):
+        get_exchange_rate("USD", "RUB", "some_key")
