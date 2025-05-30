204edc67b0efff544d4bb1b1069005bab94f8a58
Vafantule <pavelgav@gmail.com>
2025-05-30 15:23:36 +0500

    add function for read xlsx_files

--- src/fin_read.py
+++ src/fin_read.py
@@ -22,3 +22,26 @@ def read_financial_operations_from_csv_files(file_path: str) -> List[Dict[str, A
 #     csv_transactions = read_financial_operations_from_csv_files('data/transactions.csv')
 #     for transaction in csv_transactions:
 #         print(transaction)
+
+
+def read_financial_operations_from_xlsx_files(file_path: str) -> list[dict[Hashable, Any]]:
+    """
+
+    :param file_path:
+    :return:
+    """
+    dataframe = pandas.read_excel(file_path)
+    operations = dataframe.to_dict(orient="records")
+    return operations
+
+
+# if __name__ in "__main__":
+#     xlsx_transactions = read_financial_operations_from_xlsx_files('data/transactions_excel.xlsx')
+#
+#     for transaction in xlsx_transactions:
+#         if transaction.setdefault('id') == 5446796.0:
+#             print("Транзакция:")
+#             print(transaction)
+#             break
+#     else:
+#         print(f"Транзакция с id не найдена.")
